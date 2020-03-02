a5d5a2053c84c47095546ed024c8fa213b94c10b
didilehbal <habilmehdi96@gmail.com>
2020-03-02 23:14:14 +0100

    readable fix

--- src/network/DofusSocket.ts
+++ src/network/DofusSocket.ts
@@ -1,17 +1,17 @@
-import { Socket } from'net'
-import { Duplex} from 'stream'
+import { Socket } from 'net'
+import { Duplex } from 'stream'
 import Header from './Header';
 import Message from '../ankama/Message';
 
-export default class DofusSocket extends Duplex{
-    private _readingPaused :boolean;
-    private _socket:Socket;
+export default class DofusSocket extends Duplex {
+    private _readingPaused: boolean;
+    private _socket: Socket;
     constructor(socket: Socket) {
         super({ objectMode: true });
         this._readingPaused = false;
         this._socket = socket;
         this._initSocket()
-      }
+    }
     private _initSocket() {
         this._socket.on('close', hadError => this.emit('close', hadError));
         this._socket.on('connect', () => this.emit('connect'));
@@ -24,18 +24,23 @@ export default class DofusSocket extends Duplex{
         this._socket.on('readable', this._onReadable.bind(this));
     }
     _onReadable() {
-        while(!this._readingPaused){
-            const rawHeader:Buffer =this._socket.read(2)
-            if(!rawHeader)
+        while (!this._readingPaused) {
+            const rawHeader: Buffer = this._socket.read(5)
+            if (!rawHeader)
                 return;
-            
+
             const header = Header.fromRaw(rawHeader)
-         
-            const rawMsg = this._socket.read(header.length)
-            if(!rawMsg){
+
+            const howMuchLeftFromHeader = 5 - 2 - header.lenType
+            const whatLeftFromHeader = rawHeader.slice(howMuchLeftFromHeader)
+            const restOfRawMsg = this._socket.read(header.length - howMuchLeftFromHeader)
+
+            if (!restOfRawMsg) {
                 this._socket.unshift(rawHeader)
                 return
             }
+            const rawMsg = Buffer.concat([whatLeftFromHeader, restOfRawMsg])
+
             const rawPacket = Buffer.concat([header.toRaw(), rawMsg])
             let pushOk = this.push(rawPacket);
 
@@ -46,20 +51,20 @@ export default class DofusSocket extends Duplex{
     _read() {
         this._readingPaused = false;
         setImmediate(this._onReadable.bind(this));
-      }
-    _write(msg :Message,encoding:string,cb:Function) {  
+    }
+    _write(msg: Message, encoding: string, cb: Function) {
         /*let json = JSON.stringify(obj);
         let jsonBytes = Buffer.byteLength(json);
         let buffer = Buffer.alloc(4 + jsonBytes);
         buffer.writeUInt32BE(jsonBytes);
         buffer.write(json, 4);*/
         const rawMsg = msg.pack()
-        const header = new Header(msg.protocolId,0,0)
+        const header = new Header(msg.protocolId, 0, 0)
         header.length = rawMsg.length
         this._socket.write(Buffer.concat([header.toRaw(), rawMsg]));
-      }
-      _final() {
+    }
+    _final() {
         this._socket.end();
-      }
-  
+    }
+
 }
